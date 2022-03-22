5ce28b00042eab89327f8a8b55076a21518427b8
Alberto Barbosa 021 <85910024+Alberto21-boop@users.noreply.github.com>
2022-03-22 11:44:52 -0300

    Add files via upload

---
+++ placeholder/placeholder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace placeholder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string nome = "Alberto";
+            string nomeB = "Barbosa";
+            string filha = "Alicinha";
+
+            Console.WriteLine("Oi cara, seu nome é {0}, e seu sobre nome é {1}, e sua filha é a {2}", nome, nomeB, filha);
+        }
+    }
+}
